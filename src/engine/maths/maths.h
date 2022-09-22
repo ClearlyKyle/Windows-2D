80c088244408e66ff804dc0b4215f03b95911fa4
Kyle McIntosh <kylemcintosh3@hotmail.co.uk>
2022-09-22 21:21:17 +0100

    > random_int > wrap_int > wrap_float

--- src/engine/maths/maths.h
+++ src/engine/maths/maths.h
@@ -4,19 +4,50 @@
 #define _USE_MATH_DEFINES
 #include <math.h>
 
+#include <stdlib.h>
+
 #include "vec2.h"
 #include "vec3.h"
 
-double clamp_d(double d, double min, double max)
+inline double clamp_d(double d, double min, double max)
 {
     const double t = d < min ? min : d;
     return t > max ? max : t;
 }
 
-float clamp_f(float f, float min, float max)
+inline float clamp_f(float f, float min, float max)
 {
     const float t = f < min ? min : f;
     return t > max ? max : t;
+}
+
+inline int random_int(int min, int max)
+{
+    return min + rand() % (max + 1 - min);
+}
+
+inline int wrap_int(int min, int max, int value)
+{
+    if (value > max)
+        return min;
+
+    if (value < min)
+        return max;
+
+    return value;
+    // return (value > max) ? min : (value < min) ? max
+    //                                            : value;
+}
+
+inline float wrap_float(float min, float max, float value)
+{
+    if (value > max)
+        return min;
+
+    if (value < min)
+        return max;
+
+    return value;
 }
 
 #endif // __MATHS_H__
