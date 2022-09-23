ac8f5b53fbaa5ec7af11195849b35494613d2c87
Kyle McIntosh <kylemcintosh3@hotmail.co.uk>
2022-09-23 23:10:34 +0100

    physics constant settings

---
+++ src/physics/Settings.h
@@ -0,0 +1,10 @@
+#ifndef __SETTINGS_H__
+#define __SETTINGS_H__
+
+#define MIN_BODY_SIZE (0.01f * 0.01f)
+#define MAX_BODY_SIZE (64.0f * 64.0f)
+
+#define MIN_DENSITY 0.5f // g/cm^3
+#define MAX_DENSITY 21.4f
+
+#endif // __SETTINGS_H__
