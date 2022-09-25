ae3d3df0437812c0c86203efee02f72d37b525d6
Kyle McIntosh <kylemcintosh3@hotmail.co.uk>
2022-09-25 21:43:47 +0100

    App_Startup physics_test

--- src/main.c
+++ src/main.c
@@ -1,13 +1,23 @@
 #include "./engine/app.h"
 #include "snake/game.h"
+#include "physics/physics_test.h"
 
 int main(void)
 {
-    App_Startup(400,
-                400,
+    // Snake Game
+    // App_Startup(400,
+    //            400,
+    //            "Something here",
+    //            Game_init,
+    //            Game_update,
+    //            Game_on_render,
+    //            Game_on_exit);
+
+    App_Startup(1000,
+                800,
                 "Something here",
-                Game_init,
-                Game_update,
-                Game_on_render,
-                Game_on_exit);
+                Physics_init,
+                Physics_update,
+                Physics_on_render,
+                Physics_on_exit);
 }
