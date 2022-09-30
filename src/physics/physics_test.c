ed3e20e4f307e96f3f7da319ea37d98b4ed32b46
Kyle McIntosh <kylemcintosh3@hotmail.co.uk>
2022-09-30 22:49:42 +0100

    Box collision testing

--- src/physics/physics_test.c
+++ src/physics/physics_test.c
@@ -1,8 +1,17 @@
 #include "physics_test.h"
 
 #include "Rigid.h"
+#include "Collisions.h"
 
 #define NUM_OF_BODIES 10
+
+enum colour
+{
+    COL_WHITE = 0xFFFFFF,
+    COL_RED   = 0xFF0000,
+    COL_GREEN = 0x00FF00,
+    COL_BLUE  = 0x0000FF,
+};
 
 typedef struct Scene
 {
@@ -14,7 +23,7 @@ static Scene scene = {0};
 
 void Physics_init(void)
 {
-    const unsigned int padding  = 20;
+    const unsigned int padding  = 50;
     const unsigned int screen_w = Window_Width() - padding;
     const unsigned int screen_h = Window_Height() - padding;
 
@@ -23,14 +32,14 @@ void Physics_init(void)
         // 0 - Circle
         // 1 - Box
         // const int shape_type = random_int(0, 1);
-        const int shape_type = 0;
+        const int shape_type = 1;
 
         const float x = (float)random_int(padding, screen_w);
         const float y = (float)random_int(padding, screen_h);
 
         if (shape_type == SHAPE_CIRCLE)
         {
-            scene.bodies[i] = Rigid_Circle_Init((vec3){x, y, 0.0},
+            scene.bodies[i] = Rigid_Circle_Init((vec2){x, y},
                                                 20.0f,
                                                 2.0f,
                                                 0.5f,
@@ -38,9 +47,9 @@ void Physics_init(void)
         }
         else if (shape_type == SHAPE_BOX)
         {
-            scene.bodies[i] = Rigid_Box_Init((vec3){x, y, 0.0},
-                                             40.0f,
-                                             40.0f,
+            scene.bodies[i] = Rigid_Box_Init((vec2){x, y},
+                                             50.0f,
+                                             50.0f,
                                              0.5f,
                                              0.5f,
                                              false);
@@ -56,15 +65,7 @@ void Physics_init(void)
 
 void Physics_update(const double elapsed_time_ms)
 {
-    for (size_t i = 0; i < NUM_OF_BODIES; i++)
-    {
-        Rigid *body = &scene.bodies[i];
-
-        // Rigid_Move_Amount(&body->position, (vec3){0.0f, 0.1f, 0.0f});
-
-        body->position.x = wrap_float(20.0f, (float)Window_Width() - 20.0f, body->position.x);
-        body->position.y = wrap_float(20.0f, (float)Window_Height() - 20.0f, body->position.y);
-    }
+    const double elapsed_time_second = elapsed_time_ms / 1000.0;
 
     float       dx    = 0.0f;
     float       dy    = 0.0f;
@@ -93,13 +94,76 @@ void Physics_update(const double elapsed_time_ms)
         vec2 direction = {dx, dy};
         direction      = vec2_normalise(direction);
 
-        const float time = (float)(elapsed_time_ms / 1000); // ms -> s
-
         vec2 velocity = vec2_mul_scal(direction, speed);
-        velocity      = vec2_mul_scal(velocity, time); // m/s
+        velocity      = vec2_mul_scal(velocity, (float)elapsed_time_second); // m/s
 
         // First body in the list will be user controller
         Rigid_Move_Amount_2D(&scene.bodies[0].position, velocity.x, velocity.y);
+    }
+
+    // Transform Box verts
+    // const float angle = (float)(M_PI_2 * elapsed_time_second);
+    const float angle = 0.0f;
+    for (size_t i = 0; i < NUM_OF_BODIES; i++)
+    {
+        Rigid *body  = &scene.bodies[i];
+        vec2  *verts = body->verticies;
+
+        verts[0] = vec2_transform(verts[0], angle);
+        verts[1] = vec2_transform(verts[1], angle);
+        verts[2] = vec2_transform(verts[2], angle);
+        verts[3] = vec2_transform(verts[3], angle);
+
+        body->transformed_verticies[0] = vec2_add(verts[0], body->position);
+        body->transformed_verticies[1] = vec2_add(verts[1], body->position);
+        body->transformed_verticies[2] = vec2_add(verts[2], body->position);
+        body->transformed_verticies[3] = vec2_add(verts[3], body->position);
+    }
+
+    // COLLISIONS
+    // Compare each object one time, and not itself
+    for (size_t i = 0; i < NUM_OF_BODIES - 1; i++)
+    {
+        Rigid *body1         = &scene.bodies[i];
+        scene.body_colour[i] = COL_WHITE;
+
+        for (size_t j = i + 1; j < NUM_OF_BODIES; j++)
+        {
+            Rigid *body2  = &scene.bodies[j];
+            vec2   normal = {0};
+            float  depth  = 0.0f;
+
+            scene.body_colour[j] = COL_WHITE;
+
+            if (Box_intersection(*body1, *body2, &normal, &depth))
+            {
+                scene.body_colour[i] = COL_RED;
+                scene.body_colour[j] = COL_RED;
+
+                const vec2 move_amount = vec2_mul_scal(normal, depth / 2);
+
+                Rigid_Move_Amount_2D(&body1->position, -move_amount.x, -move_amount.y);
+                Rigid_Move_Amount_2D(&body2->position, move_amount.x, move_amount.y);
+            }
+
+            // if (Circle_intersection(*body1, *body2, &normal, &depth))
+            //{
+            //     const vec2 move_amount = vec2_mul_scal(normal, depth / 2);
+
+            //    Rigid_Move_Amount_2D(&body1->position, -move_amount.x, -move_amount.y);
+            //    Rigid_Move_Amount_2D(&body2->position, move_amount.x, move_amount.y);
+            //}
+        }
+    }
+
+    // Screen Wrapping
+    for (size_t i = 0; i < NUM_OF_BODIES; i++)
+    {
+        Rigid *body = &scene.bodies[i];
+        // Rigid_Move_Amount(&body->position, (vec3){0.0f, 0.1f, 0.0f});
+
+        body->position.x = wrap_float(50.0f, (float)Window_Width() - 50.0f, body->position.x);
+        body->position.y = wrap_float(50.0f, (float)Window_Height() - 50.0f, body->position.y);
     }
 }
 
@@ -119,11 +183,25 @@ void Physics_on_render(void)
         }
         else if (body.type == SHAPE_BOX)
         {
-            Window_Draw_Rectangle((int)body.position.x, (int)body.position.y, (int)body.width, (int)body.height, colour);
+            // Window_Draw_Rectangle((int)body.position.x, (int)body.position.y, (int)body.width, (int)body.height, colour);
+            const unsigned int *ind   = body.indicies;
+            vec2               *verts = body.transformed_verticies;
+
+            const vec2 v0 = verts[0];
+            const vec2 v1 = verts[1];
+            const vec2 v2 = verts[2];
+            const vec2 v3 = verts[3];
+
+            Window_Draw_Triangle(v0, v1, v2, colour);
+            Window_Draw_Triangle(v0, v2, v3, colour);
         }
     }
 }
 
 void Physics_on_exit(void)
 {
+    for (size_t i = 0; i < NUM_OF_BODIES; i++)
+    {
+        Rigid_Destroy(&scene.bodies[i]);
+    }
 }
--- src/physics/physics_test.h
+++ src/physics/physics_test.h
@@ -2,9 +2,10 @@
 #define __PHYSICS_TEST_H__
 
 #include "../engine/app.h"
+#include "../engine/input.h"
 
 void Physics_init(void);
-void Physics_update(const double elapsed_time);
+void Physics_update(const double elapsed_time_ms);
 void Physics_on_render(void);
 void Physics_on_exit(void);
 
