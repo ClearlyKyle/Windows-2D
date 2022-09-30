4a5ad7c4c4ff7f51862d71eb939565807b68be3e
Kyle McIntosh <kylemcintosh3@hotmail.co.uk>
2022-09-30 20:39:14 +0100

    Box_intersection

--- src/physics/Collisions.h
+++ src/physics/Collisions.h
@@ -25,4 +25,69 @@ bool Circle_intersection(const Rigid circle1, const Rigid circle2, vec2 *normal,
     return true;
 }
 
+inline static void _project_verticies(const vec2 *verticies, const size_t num_verts, const vec2 axis, float *min, float *max)
+{
+    *min = FLT_MAX;
+    *max = FLT_MIN;
+
+    for (size_t i = 0; i < num_verts; i++)
+    {
+        vec2        vert       = verticies[i];
+        const float projection = vec2_dot(vert, axis);
+
+        if (projection < *min)
+            *min = projection;
+        if (projection > *max)
+            *max = projection;
+    }
+}
+
+bool Box_intersection(const Rigid box1, const Rigid box2, vec2 *normal, float *depth)
+{
+    assert(box1.type == SHAPE_BOX && "Box_intersection - box1 is not a BOX\n");
+    assert(box2.type == SHAPE_BOX && "Box_intersection - box2 is not a BOX\n");
+
+    float min1, max1;
+    float min2, max2;
+
+    *normal = (vec2){0.0f, 0.0f};
+    *depth  = 0.0f;
+
+    for (size_t i = 0; i < box1.vert_count; i++)
+    {
+        const vec2 vert1 = box1.transformed_verticies[i];
+        const vec2 vert2 = box1.transformed_verticies[(i + 1) % box1.vert_count];
+
+        const vec2 edge = vec2_sub(vert2, vert1);
+        const vec2 axis = (vec2){-edge.y, edge.x};
+
+        _project_verticies(box1.transformed_verticies, box1.vert_count, axis, &min1, &max1);
+        _project_verticies(box2.transformed_verticies, box2.vert_count, axis, &min2, &max2);
+
+        if (min1 >= max2 || min2 >= max1)
+        {
+            return false; // seperated
+        }
+    }
+
+    for (size_t i = 0; i < box2.vert_count; i++)
+    {
+        const vec2 vert1 = box2.transformed_verticies[i];
+        const vec2 vert2 = box2.transformed_verticies[(i + 1) % box2.vert_count];
+
+        const vec2 edge = vec2_sub(vert2, vert1);
+        const vec2 axis = (vec2){-edge.y, edge.x};
+
+        _project_verticies(box1.transformed_verticies, box1.vert_count, axis, &min1, &max1);
+        _project_verticies(box2.transformed_verticies, box2.vert_count, axis, &min2, &max2);
+
+        if (min1 >= max2 || min2 >= max1)
+        {
+            return false; // seperated
+        }
+    }
+
+    return true; // touching
+}
+
 #endif // __COLLISIONS_H__
