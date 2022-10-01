5832e75eb9dc0dd640a5739ab58c296ba6cee092
Kyle McIntosh <kylemcintosh3@hotmail.co.uk>
2022-10-01 22:35:32 +0100

    > Box_Circle_collision

--- src/physics/Collisions.h
+++ src/physics/Collisions.h
@@ -118,4 +118,123 @@ bool Box_intersection(const Rigid box1, const Rigid box2, vec2 *normal, float *d
     return true; // touching
 }
 
+static void _project_circle(const vec2 center, const float radius, const vec2 axis, float *min, float *max)
+{
+    const vec2 direction            = vec2_normalise(axis);
+    const vec2 direction_and_radius = vec2_mul_scal(direction, radius);
+
+    const vec2 p1 = vec2_add(center, direction_and_radius);
+    const vec2 p2 = vec2_sub(center, direction_and_radius);
+
+    *min = vec2_dot(p1, axis);
+    *max = vec2_dot(p2, axis);
+
+    // Swap if they are wrong way round?
+    if (*min > *max)
+    {
+        float t = *min;
+        *min    = *max;
+        *max    = t;
+    }
+}
+
+static int _find_closest_point(const vec2 circle_center, const vec2 *verticies, const size_t num_verts)
+{
+    int   result       = -1;
+    float min_distance = FLT_MAX;
+
+    for (size_t i = 0; i < num_verts; i++)
+    {
+        const vec2  v        = verticies[i];
+        const float distance = vec2_distance(v, circle_center);
+
+        if (distance < min_distance)
+        {
+            min_distance = distance;
+            result       = (int)i;
+        }
+    }
+
+    return result;
+}
+
+bool Box_Circle_collision(const Rigid box, const Rigid circle, vec2 *normal, float *depth)
+{
+    assert(box.type == SHAPE_BOX && "Box_Circle_collision - box is not a BOX\n");
+    assert(circle.type == SHAPE_CIRCLE && "Box_Circle_collision - circle is not a BOX\n");
+
+    float min1, max1;
+    float min2, max2;
+
+    const vec2  circle_center = circle.position;
+    const float circle_radius = circle.radius;
+
+    vec2  local_normal = (vec2){0.0f, 0.0f};
+    float local_depth  = FLT_MAX;
+
+    vec2  axis       = {0};
+    float axis_depth = 0.0f;
+
+    for (size_t i = 0; i < box.vert_count; i++)
+    {
+        const vec2 vert1 = box.transformed_verticies[i];
+        const vec2 vert2 = box.transformed_verticies[(i + 1) % box.vert_count];
+
+        const vec2 edge = vec2_sub(vert2, vert1);
+        axis            = (vec2){-edge.y, edge.x};
+
+        _project_verticies(box.transformed_verticies, box.vert_count, axis, &min1, &max1);
+        _project_circle(circle_center, circle_radius, axis, &min2, &max2);
+
+        if (min1 >= max2 || min2 >= max1)
+        {
+            return false; // seperated
+        }
+
+        axis_depth = min((max2 - min1), (max1 - min2));
+
+        if (axis_depth < local_depth)
+        {
+            local_depth  = axis_depth;
+            local_normal = axis;
+        }
+    }
+
+    const int  closest_point_index = _find_closest_point(circle_center, box.verticies, box.vert_count);
+    const vec2 closest_point       = box.verticies[closest_point_index];
+
+    axis = vec2_sub(closest_point, circle_center);
+    axis = vec2_normalise(axis);
+
+    _project_verticies(box.transformed_verticies, box.vert_count, axis, &min1, &max1);
+    _project_circle(circle_center, circle_radius, axis, &min2, &max2);
+
+    if (min1 >= max2 || min2 >= max1)
+    {
+        return false; // seperated
+    }
+
+    axis_depth = min((max2 - min1), (max1 - min2));
+
+    if (axis_depth < local_depth)
+    {
+        local_depth  = axis_depth;
+        local_normal = axis;
+    }
+
+    local_depth /= vec2_length(local_normal);
+    local_normal = vec2_normalise(local_normal);
+
+    const vec2 direction = vec2_sub(box.position, circle_center);
+
+    if (vec2_dot(direction, local_normal) < 0.0f)
+        *normal = VEC2_NEGATIVE(local_normal);
+    else
+        *normal = local_normal;
+
+    *depth = local_depth;
+
+    return true;
+}
+
 #endif // __COLLISIONS_H__
