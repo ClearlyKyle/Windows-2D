15de37217c714958d4ec3dcd9f967f2ec7a5da82
Kyle McIntosh <kylemcintosh3@hotmail.co.uk>
2022-10-03 16:32:30 +0100

    engine header

---
+++ src/engine/engine.h
@@ -0,0 +1,10 @@
+#ifndef __ENGINE_H__
+#define __ENGINE_H__
+
+#include "app.h"
+#include "input.h"
+
+#include "physics/Collisions/Collisions.h"
+#include "physics/Rigid.h"
+
+#endif // __ENGINE_H__
