30f2af15c00f060b95e57d3e42a942da80afb162
Kyle McIntosh <kylemcintosh3@hotmail.co.uk>
2022-10-07 22:09:40 +0100

    collision_test removed from header

--- src/engine/physics/Collisions/Collisions.c
+++ src/engine/physics/Collisions/Collisions.c
@@ -230,3 +230,40 @@ bool collision_box_circle(const Rigid box, const Rigid circle, vec2 *normal, flo
 
     return true;
 }
+
+bool collision_test(const Rigid body1, const Rigid body2, vec2 *normal, float *depth)
+{
+    const enum Shape_Type body1_type = body1.type;
+    const enum Shape_Type body2_type = body2.type;
+
+    *normal = (vec2){0.0f, 0.0f};
+    *depth  = 0.0f;
+
+    if (body1_type == SHAPE_BOX)
+    {
+        if (body2_type == SHAPE_BOX)
+        {
+            return collision_box_box(body1, body2, normal, depth);
+        }
+        else if (body2_type == SHAPE_CIRCLE)
+        {
+            return collision_box_circle(body1, body2, normal, depth);
+        }
+    }
+    if (body1_type == SHAPE_CIRCLE)
+    {
+        if (body2_type == SHAPE_BOX)
+        {
+            const bool res = collision_box_circle(body2, body1, normal, depth);
+            VEC2_NEGATIVE(*normal);
+            return res;
+        }
+        else if (body2_type == SHAPE_CIRCLE)
+        {
+            return collision_circle_circle(body1, body2, normal, depth);
+        }
+    }
+
+    fprintf(stderr, "No collision test for types : %d, %d\n", body1_type, body2_type);
+    return false;
+}
--- src/engine/physics/Collisions/Collisions.h
+++ src/engine/physics/Collisions/Collisions.h
@@ -10,39 +10,6 @@ bool collision_circle_circle(const Rigid circle1, const Rigid circle2, vec2 *nor
 bool collision_box_box(const Rigid box1, const Rigid box2, vec2 *normal, float *depth);
 bool collision_box_circle(const Rigid box, const Rigid circle, vec2 *normal, float *depth);
 
-inline bool collision_test(const Rigid body1, const Rigid body2, vec2 *normal, float *depth)
-{
-    const enum Shape_Type body1_type = body1.type;
-    const enum Shape_Type body2_type = body2.type;
-
-    *normal = (vec2){0.0f, 0.0f};
-    *depth  = 0.0f;
-
-    if (body1_type == SHAPE_BOX)
-    {
-        if (body2_type == SHAPE_BOX)
-        {
-            return collision_box_box(body1, body2, normal, depth);
-        }
-        else if (body2_type == SHAPE_CIRCLE)
-        {
-            return collision_box_circle(body1, body2, normal, depth);
-        }
-    }
-    if (body1_type == SHAPE_CIRCLE)
-    {
-        if (body2_type == SHAPE_BOX)
-        {
-            return collision_box_circle(body2, body1, normal, depth);
-        }
-        else if (body2_type == SHAPE_CIRCLE)
-        {
-            return collision_circle_circle(body1, body2, normal, depth);
-        }
-    }
-
-    fprintf(stderr, "No collision test for types : %d, %d\n", body1_type, body2_type);
-    return false;
-}
+bool collision_test(const Rigid body1, const Rigid body2, vec2 *normal, float *depth);
 
 #endif // __COLLISIONS_H__
