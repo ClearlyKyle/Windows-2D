e0d67833d3e2f0b0bcdcbf4f009890b8c943481c
Kyle McIntosh <kylemcintosh3@hotmail.co.uk>
2022-10-12 21:29:22 +0100

    increase MAX_BODY_SIZE

--- src/engine/physics/Settings.h
+++ src/engine/physics/Settings.h
@@ -2,7 +2,7 @@
 #define __SETTINGS_H__
 
 #define MIN_BODY_SIZE (0.01f * 0.01f)
-#define MAX_BODY_SIZE (64.0f * 64.0f)
+#define MAX_BODY_SIZE (640.0f * 640.0f)
 
 #define MIN_DENSITY 0.5f // g/cm^3
 #define MAX_DENSITY 21.4f
